b33f6a11720dbe5efaf64480bbc6bea0fd580bc1
wonho.kim <kwhk1993@naver.com>
2017-01-05 14:13:51 +0900

    게임타임과 씬 클래스

---
+++ 따라하기 04- 게임 타임과 씬 추가/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <mmsystem.h>					//시간 관련 라이브러리
+#include <math.h>
+#include <stdio.h>
+
+#include <d3d11.h>
+#include <d3dx11.h>
+#define FRAME_BUFFER_WIDTH	640
+#define FRAME_BUFFER_HEIGHT	480
+// TODO: reference additional headers your program requires here
