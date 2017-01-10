e77e5bf1e5fddf9cd084691570cb11afee70a91a
wonho.kim <kwhk1993@naver.com>
2017-01-10 11:35:54 +0900

    따라하기 12 - 직육면체 여러개

---
+++ fxcc_11.bat
@@ -0,0 +1,5 @@
+set PATH=c:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\
+	cd 따라하기 011 - 회전하는 직육면체 삼각형 스트립
+	fxc /Od /Zi /Ges /T vs_5_0 /E VS /Fo VS.fxo VS.fx
+	fxc /Od /Zi /Ges /T ps_5_0 /E PS /Fo PS.fxo PS.fx
+	cd ..
