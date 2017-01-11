16654469b334767849b2764973bad2dc598fca18
wonho.kim <kwhk1993@naver.com>
2017-01-11 11:04:02 +0900

    따라하기 012 - 카메라

---
+++ fxcc_12.bat
@@ -0,0 +1,5 @@
+set PATH=c:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\
+	cd 따라하기 012 - 카메라
+	fxc /Od /Zi /Ges /T vs_5_0 /E VS /Fo VS.fxo VS.fx
+	fxc /Od /Zi /Ges /T ps_5_0 /E PS /Fo PS.fxo PS.fx
+	cd ..
